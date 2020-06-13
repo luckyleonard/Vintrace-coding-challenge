feaf0918b1f7af0bd9b482c64508b0a6c342ed29
luckyleonard <dzhu31@hotmail.com>
2020-06-13 18:44:59 +1000

    ✨ feat(WineList) build WineList Container to show the list of wines

--- src/containers/WineList/WineList.jsx
+++ src/containers/WineList/WineList.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { List } from 'antd';
+import { Link } from 'react-router-dom';
+import winePic from '../../assets/images/wine.svg';
 
-function WineList() {
-  return <div>Wine list</div>;
+const StyledItem = styled.div`
+  background: #adc9ad;
+  border: 1px solid #fff;
+`;
+
+function WineList({ winesData }) {
+  return (
+    <List
+      itemLayout='horizontal'
+      dataSource={winesData}
+      renderItem={(item) => (
+        <StyledItem>
+          <Link to={`/wine/${item.lotCode}`}>
+            <List.Item>
+              <List.Item.Meta
+                avatar={winePic}
+                title={item.lotCode}
+                description={
+                  item.description ? item.description : 'No description'
+                }
+              />
+            </List.Item>
+          </Link>
+        </StyledItem>
+      )}
+    />
+  );
 }
 
 export default WineList;
